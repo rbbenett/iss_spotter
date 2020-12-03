bbfa018fc1e133a4934162f99693158dd619cf8a
Robert <rbbenett0614@gmail.com>
2020-12-03 23:05:50 +0000

    Fourth Commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,3 +9,11 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+// fetchCoordsByIP('67.38.165.77', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//   }
+
+//   console.log("It worked! Returned Coords:", coords);
+// });
--- iss.js
+++ iss.js
@@ -1,11 +1,3 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json',  (error, response, body) => {
@@ -15,10 +7,32 @@ const fetchMyIP = function(callback) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
       return;
     }
-    
+
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`,  (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const lat = JSON.parse(body).latitude;
+    const long = JSON.parse(body).longitude;
+    let obj = {
+      latitude: lat,
+      longitude: long
+    };
+    
+    callback(null, obj);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
