e363dce3ae44406cd5dfedb6406e9e5479691388
Robert <rbbenett0614@gmail.com>
2020-12-04 00:51:58 +0000

    Final Commit

--- index.js
+++ index.js
@@ -44,3 +44,5 @@ nextISSTimesForMyLocation((error, passTimes) => {
   }
   printPassTimes(passTimes);
 });
+
+module.exports = { printPassTimes };
---
+++ index2.js
@@ -0,0 +1,15 @@
+const { nextISSTimesForMyLocation, printPassTimes } = require('./iss_promised');
+
+
+// fetchMyIp()
+// .then(fetchCoordsByIp)
+// .then(fetchISSFlyOverTimes)
+// .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
